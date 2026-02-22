b2a98300bacb54d9d2a4d1a95513e60004689656
Mojtaba zamani <memojtabazamani@gmail.com>
2026-02-23 00:06:42 +0330

    14041204
    - Connect with mongo db

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -13,6 +14,18 @@ app.use(cors({
 
 app.use(compression());
 app.use(bodyParser.json());
-app.user(cookieParser());
+app.use(cookieParser());
 
 const server = http.createServer(app)
+
+server.listen(8080, () => {
+    console.log(`Server listening on http://localhost:8080`);
+});
+
+const MONG_URL = 'mongodb+srv://crazyprogrammer1001_db_user:iuwY46nxRYVpL4dZ@cluster0.m3tl1jq.mongodb.net/?appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONG_URL);
+mongoose.connection.on('error', (err: Error) => {
+    console.error(err);
+})
